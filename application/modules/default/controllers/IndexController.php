ba9a522b4b570c189c85c54b3b97ab3609b60210
Olekhy <olekhy@googlemail.com>
2012-05-07 16:13:53 +0200

    add layout and blueprint css files

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -11,8 +11,8 @@ class Default_IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         $def = new Default_Model_Model();
-        echo $def->useStandardModel();
-        echo new Standard_Model_Standard();
+        $this->view->value1 = $def->useStandardModel() . ' by ' . get_class($def);
+        $this->view->value2 = new Standard_Model_Standard();
     }
 
 
